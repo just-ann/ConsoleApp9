82d00b464aed791ed7cc996d3cbbc6d9bbd672bd
just-ann <78508451+just-ann@users.noreply.github.com>
2021-03-01 19:06:39 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06Lib2
+{
+    public class Printer
+    {
+        public string Name;
+        public string Appointment;
+        public string Printing_technology;
+        public int Buyers;
+        public double YearIncome;
+        public double Square;
+        public bool HasPhotoprinting;
+        public bool HasTwo_sided_printing;
+        public double yearIncomePerBuyer
+        {
+            get
+            {
+                return GetYearIncomePerBuyer();
+            }
+        }
+        public double GetYearIncomePerBuyer()
+        {
+            return YearIncome / Buyers;
+        }
+
+    }
+}
